4cb96b9677b8779dd8608365a9c1cccdb59ada2e
Hansen06 <2696371886@qq.com>
2018-03-14 20:30:21 +0800

    homework

---
+++ src/Offer/Solution004.java
@@ -0,0 +1,15 @@
+package Offer;
+
+public class Solution004 {
+
+    public int JumpFloorII(int target) {
+        if(target == 0){
+            return 0;
+        }
+        if(target == 1){
+            return 1;
+        }
+        return 1;
+    }
+
+}
