ed913ec0825af9ff7b96a855c9437a0fc4f5a002
Hansen06 <2696371886@qq.com>
2018-03-26 20:33:46 +0800

    main

---
+++ src/Online/Main001.java
@@ -0,0 +1,36 @@
+package Online;
+
+import java.util.Scanner;
+
+public class Main001 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        int[] x = new int[4];
+        int[] y = new int[4];
+
+        for (int i = 0; i < t; i++) {
+            for (int j = 0; j < 4; j++) {
+                x[j] = sc.nextInt();
+            }
+            for (int j = 0; j < 4; j++) {
+                y[j] = sc.nextInt();
+            }
+
+            int a = (x[0]-x[1])*(x[0]-x[1]) + (y[0]-y[1])*(y[0]-y[1]);
+            int b = (x[0]-x[2])*(x[0]-x[2]) + (y[0]-y[2])*(y[0]-y[2]);
+            int c = (x[1]-x[2])*(x[1]-x[2]) + (y[1]-y[2])*(y[1]-y[2]);
+
+
+            if(a+b == c){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+
+    }
+
+}
---
+++ src/Online/Main002.java
@@ -0,0 +1,8 @@
+package Online;
+
+public class Main002 {
+
+    public static void main(String[] args) {
+
+    }
+}
