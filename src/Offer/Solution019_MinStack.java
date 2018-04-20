838c2c186e55d3411e988a835dc882469ade540e
Hansen06 <2696371886@qq.com>
2018-04-20 16:21:59 +0800

    minstack

---
+++ src/Offer/Solution019_MinStack.java
@@ -0,0 +1,54 @@
+package Offer;
+
+import java.util.Stack;
+
+public class Solution019_MinStack {
+
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> stackMin = new Stack<>();
+
+    public void push(int node) {
+        if (stackMin.isEmpty()) {
+            stackMin.push(node);
+        } else if (node < min()) {
+            stackMin.push(node);
+        }else {
+            int min = stackMin.peek();
+            stackMin.push(min);
+        }
+        stack.push(node);
+    }
+
+    public void pop() {
+        stack.pop();
+        stackMin.pop();
+    }
+
+    public void push1(int node) {
+        if (stackMin.isEmpty()) {
+            stackMin.push(node);
+        } else if (node <= min()) {
+            stackMin.push(node);
+        }
+        stack.push(node);
+    }
+
+    public void pop1() {
+        int value = stack.pop();
+        if (value == stackMin.pop()){
+            stackMin.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return stackMin.peek();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
