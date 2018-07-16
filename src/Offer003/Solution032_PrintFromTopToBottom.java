bb781d0b304763effc1adb65c4bfae1efb9de711
Hansen06 <2696371886@qq.com>
2018-07-16 22:47:48 +0800

    0716

---
+++ src/Offer003/Solution032_PrintFromTopToBottom.java
@@ -0,0 +1,44 @@
+package Offer003;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 从上往下打印二叉树
+ */
+
+public class Solution032_PrintFromTopToBottom {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode tmp = queue.poll();
+            result.add(tmp.val);
+            if (tmp.left != null) {
+                queue.add(tmp.left);
+            }
+            if (tmp.right != null) {
+                queue.add(tmp.right);
+            }
+        }
+        return result;
+    }
+}
