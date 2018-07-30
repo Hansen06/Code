a0a0f129c0cff3995e11aad2532b8d89795db6cc
Hansen06 <2696371886@qq.com>
2018-07-30 22:53:16 +0800

    Maximum Depth of Binary Tree

--- src/leetcode_depth_first_search/Solution0104_MaximumDepthofBinaryTree.java
+++ src/leetcode_depth_first_search/Solution0104_MaximumDepthofBinaryTree.java
@@ -14,7 +14,7 @@ public class Solution0104_MaximumDepthofBinaryTree {
         }
     }
 
-    public int maxDepth(TreeNode root) {
+    public static int maxDepth(TreeNode root) {
         if (root == null){
             return 0;
         }
@@ -24,4 +24,20 @@ public class Solution0104_MaximumDepthofBinaryTree {
         return (left > right) ? left + 1: right + 1;
     }
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(6);
+
+        root.left = node1;
+        root.right = node2;
+
+        node1.left = node3;
+        node1.right = node4;
+
+        System.out.println(maxDepth(root));
+    }
 }
