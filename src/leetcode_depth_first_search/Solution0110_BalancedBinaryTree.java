ad9dd4041ff351c890ef0487a810c2044a0193a2
Hansen06 <2696371886@qq.com>
2018-08-16 22:17:05 +0800

    Balanced Binary Tree

---
+++ src/leetcode_depth_first_search/Solution0110_BalancedBinaryTree.java
@@ -0,0 +1,37 @@
+package leetcode_depth_first_search;
+
+/**
+ * Balanced Binary Tree
+ */
+public class Solution0110_BalancedBinaryTree {
+
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            this.val = x;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        int left = hightTree(root);
+        int right = hightTree(root);
+        if ((left-right) > 1 && (right - left) < -1){
+            return false;
+        }
+        return true;
+    }
+
+    public int hightTree(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = hightTree(root.left);
+        int right = hightTree(root.right);
+        return (left > right) ? left + 1 : right + 1;
+    }
+}
