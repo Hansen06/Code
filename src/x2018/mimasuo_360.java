810cf6a37f86556ddc4db94b08bcf5f5801bec73
Hansen06 <haishenmuc@163.com>
2018-08-27 17:18:49 +0800

    密码锁

---
+++ src/x2018/mimasuo_360.java
@@ -0,0 +1,38 @@
+package x2018;
+
+import java.util.Scanner;
+
+/**
+ * 赛码 360 最强大脑
+ */
+public class mimasuo_360 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String fir = sc.next();
+            String sec = sc.next();
+            String th = sc.next();
+            boolean isCen = false;
+            isCen = judge(fir,sec,th);
+            if (isCen){
+                System.out.println("YES");
+            }else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+
+    private static boolean judge(String fir, String sec, String th) {
+        if (fir == null && th == null && sec == null){
+            return false;
+        }
+        boolean f1 = fir.substring(0,1).equals(th.substring(2,3));
+        boolean f2 = fir.substring(1,2).equals(th.substring(1,2));
+        boolean f3 = fir.substring(2,3).equals(th.substring(0,1));
+        boolean f4 = sec.substring(0,1).equals(sec.substring(2,3));
+        return f1 & f2 & f3 & f4;
+    }
+
+}
