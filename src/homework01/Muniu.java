7a955ac01d27b63262e00920a537a655f59621dc
Hansen06 <haishenmuc@163.com>
2018-09-01 11:01:59 +0800

    update

--- src/homework01/Muniu.java
+++ src/homework01/Muniu.java
@@ -8,15 +8,40 @@ import java.util.Scanner;
  * 每年年初也生一头小母牛。请编程实现在第n年的时候，共有多少头母牛？
  */
 public class Muniu {
+
+    public static int f1(int n) {
+        if (n == 0){
+            return 0;
+        }
+        int[] a = new int[n+1];
+        if (n == 1){
+            return 1;
+        }else if (n == 2){
+            return 2;
+        }else if (n == 3){
+            return 3;
+        }else {
+            a[1] = 1;
+            a[2] = 2;
+            a[3] = 3;
+            for (int i = 4;i <= n; i++) {
+                a[i] = a[i-1] + a[i-3];
+            }
+            return a[n];
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextInt()) {
             int n = sc.nextInt();
-            System.out.println(f(n));
+            System.out.println(f1(n));
         }
+        sc.close();
     }
 
-    private static int f(int n) {
+    /**复杂度过高
+    public static int f(int n) {
         if(n == 1){
             return 1;
         }else if(n == 2){
@@ -27,4 +52,5 @@ public class Muniu {
             return f(n-1) + f(n-3);
         }
     }
+     **/
 }
