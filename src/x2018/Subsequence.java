7f332f9da2a36af584dfafd41ddadff669468694
Hansen06 <haishenmuc@163.com>
2018-09-22 09:39:55 +0800

    update

--- src/x2018/Subsequence.java
+++ src/x2018/Subsequence.java
@@ -14,13 +14,13 @@ public class Subsequence {
     }
 
     private static void getSub(String str, int pos, StringBuilder sb) {
-        if (pos == str.length()){
+        if (pos == str.length()) {
             System.out.println(sb.toString());
             return;
         }
 
         getSub(str, pos + 1, sb.append(str.charAt(pos)));
-        sb.deleteCharAt(sb.length()-1);
+        sb.deleteCharAt(sb.length() - 1);
         getSub(str, pos + 1, sb);
     }
 }
