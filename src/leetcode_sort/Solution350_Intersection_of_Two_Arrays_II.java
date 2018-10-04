6f2a6120bcc5c2e9e8d6862c6e9d52a5f46731be
Hansen06 <haishenmuc@163.com>
2018-10-04 23:08:57 +0800

    update

--- src/leetcode_sort/Solution350_Intersection_of_Two_Arrays_II.java
+++ src/leetcode_sort/Solution350_Intersection_of_Two_Arrays_II.java
@@ -1,9 +1,6 @@
 package leetcode_sort;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/intersection-of-two-arrays-ii/
@@ -18,17 +15,17 @@ public class Solution350_Intersection_of_Two_Arrays_II {
         LinkedList<Integer> list1 = new LinkedList<>();
         ArrayList<Integer> res_list = new ArrayList<>();
 
-        if (nums1.length < nums2.length){
-            for (int i = 0; i < nums2.length ; i++) {
+        if (nums1.length < nums2.length) {
+            for (int i = 0; i < nums2.length; i++) {
                 list1.add(nums2[i]);
             }
-            for (int i = 0; i < nums1.length ; i++) {
-                if (list1.contains(nums1[i])){
+            for (int i = 0; i < nums1.length; i++) {
+                if (list1.contains(nums1[i])) {
                     res_list.add(nums1[i]);
                     list1.remove(list1.indexOf(nums1[i]));
                 }
             }
-        }else {
+        } else {
             for (int i = 0; i < nums1.length; i++) {
                 list1.add(nums1[i]);
             }
@@ -43,13 +40,39 @@ public class Solution350_Intersection_of_Two_Arrays_II {
         int[] res = new int[res_list.size()];
         int i = 0;
         for (int j = 0; j < res_list.size(); j++) {
-            res[i++] = (int)res_list.get(j);
+            res[i++] = (int) res_list.get(j);
         }
         return res;
     }
 
+    public static int[] intersect1(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
+            }
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                result.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+
+        int[] res = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            res[i] = result.get(i);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
-        int[] nums1 = {2,1}, nums2 = {1,1};
+        int[] nums1 = {2, 1}, nums2 = {1, 1};
 //        int[] nums1 = {4,9,5}, nums2 = {9,4,9,8,4};
         int[] res = intersect(nums1, nums2);
         for (int i = 0; i < res.length; i++) {
