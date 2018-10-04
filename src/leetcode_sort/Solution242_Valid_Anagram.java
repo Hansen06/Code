2248ac0b7221061afb58aabec859931e8f44b614
Hansen06 <haishenmuc@163.com>
2018-10-04 15:06:24 +0800

    update

--- src/leetcode_sort/Solution242_Valid_Anagram.java
+++ src/leetcode_sort/Solution242_Valid_Anagram.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * https://leetcode.com/problems/valid-anagram/
  */
 public class Solution242_Valid_Anagram {
+
     public static boolean isAnagram(String s, String t) {
         if (s == null || t == null || s.length() != t.length()) {
             return false;
@@ -23,9 +24,38 @@ public class Solution242_Valid_Anagram {
         return true;
     }
 
+    /**
+     * 哈希
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isAnagram1(String s, String t) {
+        if (s == null || t == null || s.length() != t.length()) {
+            return false;
+        }
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        int[] set = new int[26];
+        for (int i = 0; i < ch1.length; i++) {
+            set[ch1[i] - 'a']++;
+        }
+        for (int i = 0; i < ch2.length; i++) {
+            set[ch2[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (set[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String s = "anagram";
         String t = "nagaram";
         System.out.println(isAnagram(s, t));
+        System.out.println(isAnagram1(s, t));
     }
 }
