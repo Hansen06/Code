69b34f0631d3af07bdfc08f8b840382de67ee85b
Hansen06 <haishenmuc@163.com>
2018-11-05 22:11:39 +0800

    Solution866_Prime_Palindrome

---
+++ src/leetcode/Solution866_Prime_Palindrome.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/prime-palindrome/description/
+ */
+public class Solution866_Prime_Palindrome {
+
+    /**
+     * 找到比N大的最小的奇数回文
+     *
+     * @param N
+     * @return
+     */
+    public static int primePalindrome(int N) {
+        if (8 <= N && N <= 11) {
+            return 11;
+        }
+        for (int x = 1; x < 100000; x++) {
+            String s = Integer.toString(x);
+            String r = new StringBuilder(s).reverse().toString().substring(1);
+            int y = Integer.parseInt(s + r);
+            if (y >= N && isPrime(y)) {
+                return y;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean isPrime(int x) {
+        if (x < 2 || x % 2 == 0) {
+            return x == 2;
+        }
+        for (int i = 3; i * i <= x; i += 2) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println(primePalindrome(n));
+    }
+}
